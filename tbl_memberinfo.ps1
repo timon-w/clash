9d63f6a78874a750def819a52f3a98e651149b67
timon-w <timonw@gmail.com>
2016-04-20 11:29:58 +1200

    Update tbl_memberinfo.ps1

--- tbl_memberinfo.ps1
+++ tbl_memberinfo.ps1
@@ -8,16 +8,16 @@ $requesturl = "https://api.clashofclans.com/v1/clans/"+ $clantag + "/members"
 $headers = @{"authorization"=("Bearer " + $token)}
 $members = (Invoke-WebRequest -Method get -uri $requesturl -Headers $headers | ConvertFrom-Json).items
 
-$sqlquery1 = "execute sp_cleartbl_memberinfo_temp"
-invoke-sqlcmd -Query $sqlquery1 -ServerInstance $sqlserver -Database $sqldb -username $sqlusername -password $sqlpassword
+$query1 = "execute sp_cleartbl_memberinfo_temp"
+invoke-sqlcmd -Query $query1 -ServerInstance $sqlserver -Database $sqldb -username $sqlusername -password $sqlpassword
 
 $members | foreach {
-	$memberinfo_tag = $_.tag
-	$memberinfo_name = $_.name
-	$memberinfo_role = $_.role
-	$sqlquery2 = "insert into tbl_memberinfo_temp (memberinfo_tag, memberinfo_name, memberinfo_role) values ('$memberinfo_tag', '$memberinfo_name', '$memberinfo_role')"
-	invoke-sqlcmd -Query $sqlquery2 -ServerInstance $sqlserver -Database $sqldb -username $sqlusername -password $sqlpassword
+	$1 = $_.tag
+	$2 = $_.name
+	$3 = $_.role
+	$query2 = "insert into tbl_memberinfo_temp values ('$1', '$2', '$3')"
+	invoke-sqlcmd -Query $query2 -ServerInstance $sqlserver -Database $sqldb -username $sqlusername -password $sqlpassword
 }
 
-$sqlquery3 = "execute sp_mergetbl_memberinfo_temp"
-invoke-sqlcmd -Query $sqlquery3 -ServerInstance $sqlserver -Database $sqldb -username $sqlusername -password $sqlpassword
+$query3 = "execute sp_mergetbl_memberinfo_temp"
+invoke-sqlcmd -Query $query3 -ServerInstance $sqlserver -Database $sqldb -username $sqlusername -password $sqlpassword
