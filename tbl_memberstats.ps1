94f455e29c14444f54074e7907628087537db84d
timon-w <timonw@gmail.com>
2016-04-19 16:08:16 +1200

    Update tbl_memberstats.ps1

--- tbl_memberstats.ps1
+++ tbl_memberstats.ps1
@@ -1,10 +1,10 @@
 $token = [token]
 $clantag = [clantag]
+$sqlusername = [username]
+$sqlpassword = [password]
 $requesturl = "https://api.clashofclans.com/v1/clans/"+ $clantag + "/members"
 $headers = @{"authorization"=("Bearer " + $token)}
 $members = (Invoke-WebRequest -Method get -uri $requesturl -Headers $headers | ConvertFrom-Json).items
-$sqlusername = [username]
-$sqlpassword = [password]
 $date = get-date
 
 $members | foreach {
