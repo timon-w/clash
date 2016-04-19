d3a0e83a52bfdea434ca1086080ee1cc62f7ea37
timon-w <timonw@gmail.com>
2016-04-19 17:13:51 +1200

    Update tbl_memberinfo.ps1

--- tbl_memberinfo.ps1
+++ tbl_memberinfo.ps1
@@ -1,5 +1,7 @@
 $token = [token]
 $clantag = [clantag]
+$sqlserver = [sqlserver]
+$sqldb = [sqldb]
 $sqlusername = [username]
 $sqlpassword = [password]
 $requesturl = "https://api.clashofclans.com/v1/clans/"+ $clantag + "/members"
@@ -7,15 +9,15 @@ $headers = @{"authorization"=("Bearer " + $token)}
 $members = (Invoke-WebRequest -Method get -uri $requesturl -Headers $headers | ConvertFrom-Json).items
 
 $query1 = "execute sp_cleartbl_memberinfo_temp"
-invoke-sqlcmd -Query $query1 -ServerInstance "timon.clphw7e0y0fq.ap-southeast-2.rds.amazonaws.com" -Database clash -username $sqlusername -password $sqlpassword
+invoke-sqlcmd -Query $query1 -ServerInstance $sqlserver -Database $sqldb -username $sqlusername -password $sqlpassword
 
 $members | foreach {
 	$memberinfo_tag = $_.tag
 	$memberinfo_name = $_.name
 	$memberinfo_role = $_.role
 	$query2 = "insert into tbl_memberinfo_temp (memberinfo_tag, memberinfo_name, memberinfo_role) values ('$memberinfo_tag', '$memberinfo_name', '$memberinfo_role')"
-	invoke-sqlcmd -Query $query2 -ServerInstance "timon.clphw7e0y0fq.ap-southeast-2.rds.amazonaws.com" -Database clash -username $sqlusername -password $sqlpassword
+	invoke-sqlcmd -Query $query2 -ServerInstance $sqlserver -Database $sqldb -username $sqlusername -password $sqlpassword
 }
 
 $query3 = "execute sp_mergetbl_memberinfo_temp"
-invoke-sqlcmd -Query $query3 -ServerInstance "timon.clphw7e0y0fq.ap-southeast-2.rds.amazonaws.com" -Database clash -username $sqlusername -password $sqlpassword
+invoke-sqlcmd -Query $query3 -ServerInstance $sqlserver -Database $sqldb -username $sqlusername -password $sqlpassword
