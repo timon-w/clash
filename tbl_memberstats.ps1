682a9772de8370e09dc6c670aa75f0a78d53f369
timon-w <timonw@gmail.com>
2016-04-19 17:14:43 +1200

    Update tbl_memberstats.ps1

--- tbl_memberstats.ps1
+++ tbl_memberstats.ps1
@@ -1,5 +1,7 @@
 $token = [token]
 $clantag = [clantag]
+$sqlserver = [sqlserver]
+$sqldb = [sqldb]
 $sqlusername = [username]
 $sqlpassword = [password]
 $requesturl = "https://api.clashofclans.com/v1/clans/"+ $clantag + "/members"
@@ -17,7 +19,7 @@ $members | foreach {
   $memberstats_donations = $_.donations
   $memberstats_donationsrec = $_.donationsreceived
   
-  $query = "insert into tbl_memberstats (memberstats_date, memberstats_tag, memberstats_explevel, memberstats_trophies, memberstats_clanrank, memberstats_prevclankrank, memberstats_donations, memberstats_donationsrec) values ('$memberstats_date', '$memberstats_tag', '$memberstats_explevel', '$memberstats_trophies', '$memberstats_clanrank', '$memberstats_prevclankrank', '$memberstats_donations', '$memberstats_donationsrec')"
+  $sqlquery1 = "insert into tbl_memberstats (memberstats_date, memberstats_tag, memberstats_explevel, memberstats_trophies, memberstats_clanrank, memberstats_prevclankrank, memberstats_donations, memberstats_donationsrec) values ('$memberstats_date', '$memberstats_tag', '$memberstats_explevel', '$memberstats_trophies', '$memberstats_clanrank', '$memberstats_prevclankrank', '$memberstats_donations', '$memberstats_donationsrec')"
 
-  invoke-sqlcmd -Query $query -ServerInstance "timon.clphw7e0y0fq.ap-southeast-2.rds.amazonaws.com" -Database clash -Username $sqlusername -Password $sqlpassword
+  invoke-sqlcmd -Query $sqlquery1 -ServerInstance $sqlserver -Database $sqldb -Username $sqlusername -Password $sqlpassword
 }
