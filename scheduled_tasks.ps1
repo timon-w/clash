a93e0c57cf03852736e5fa183c574055f709b32d
timon-w <timonw@gmail.com>
2016-04-20 14:41:08 +1200

    Update scheduled_tasks.ps1

--- scheduled_tasks.ps1
+++ scheduled_tasks.ps1
@@ -8,7 +8,7 @@ $tasks = import-csv $csvpath
 
 $tasks | foreach{
 	$trigger =  New-ScheduledTaskTrigger -Daily -At $_.time
-	$arguement = "-file `"$path$_.task`""
+	$arguement = "-file `""+$path+$_.task+"`""
 	$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument $arguement
 	Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $_.task -taskpath $taskpath -user $username -password $password
 }
